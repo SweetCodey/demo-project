e57ce22ea7b5639f18ef726d369b19307c8f83eb
Suresh Gandhi <sureshgandhi@Sureshs-MacBook-Pro.local>
2025-09-01 16:22:13 -0400

    Add get ready to creating flappy bird text

--- src/App.jsx
+++ src/App.jsx
@@ -16,7 +16,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
+      <h1>Get Ready to make the Flappy Bird Game!</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
